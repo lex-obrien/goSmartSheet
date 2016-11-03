f2659c549c7feb23dfd31d96b515ebe0dcdbb92f
Lex O'Brien <lex.obrien@gmail.com>
2016-11-02 23:54:33 -0500

    Added new post options for adding rows

--- client.go
+++ client.go
@@ -143,19 +143,6 @@ func (c *Client) Get(path string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-//RowPostOptions is used in conjuction with Adding a Row to a sheet to specific location
-type RowPostOptions int16
-
-const (
-	//ToTop will Add or move the Row to the top of the Sheet.
-	ToTop RowPostOptions = iota
-	//ToBottom will Add or move the Row to the bottom of the Sheet.
-	ToBottom
-	//Above will Add or move the Row directly above the specified sibling Row (at the same hierarchical level).
-	//Sibling Row must be populated for this option to work
-	Above
-)
-
 func (c *Client) AddRowToSheet(sheetID string, opt RowPostOptions, cellValues ...CellValue) (io.ReadCloser, error) {
 
 	//TODO: validate length of cols with cells, match types, etc
@@ -167,8 +154,10 @@ func (c *Client) AddRowToSheet(sheetID string, opt RowPostOptions, cellValues ..
 		log.Fatalf("Error: %v\n", err)
 	}
 
+	//TODO: apply multi-row validation logic here as well
+
 	if len(cols) != len(cellValues) {
-		log.Fatalf("Data Value length must match columsn in sheet\n")
+		log.Fatalf("Data Value length must match columns in sheet\n")
 		return nil, nil //TODO: make an actual error
 	}
 
@@ -195,34 +184,46 @@ func (c *Client) AddRowToSheet(sheetID string, opt RowPostOptions, cellValues ..
 	return body, nil
 }
 
-func (c *Client) AddRowsToSheet(sheetID string, opt RowPostOptions, rows []Row) (io.ReadCloser, error) {
+func (c *Client) AddRowsToSheet(sheetID string, rowOpt RowPostOptions, rows []Row, opt PostOptions) (io.ReadCloser, error) {
 
 	//TODO: validate length of cols with cells, match types, etc
 	//right now this assumes the consumer is putting them in the correct order
 
-	cols, err := c.GetColumns(sheetID)
-	if err != nil {
-		log.Fatalf("Error: %v\n", err)
-	}
-
 	//adjust each row to match values
+	var sheetCols []Column
+	var err error
+	var colsPopulated bool
 	for i := range rows {
-
-		//TODO: still confused why I need this?  Assuming because the range copies things still....
 		r := &rows[i]
-
-		if len(cols) != len(r.Cells) {
-			log.Fatalf("Data Value length must match columsn in sheet\n")
-			return nil, nil //TODO: make an actual error
-		}
 
 		//adjust col IDs
 		for j := range r.Cells {
-			r.Cells[j].ColumnID = cols[j].ID
+			//only fill the col Id if they are missing
+			if r.Cells[j].ColumnID == 0 {
+				//columnId is missing, so we need to perform some validation
+
+				if !colsPopulated {
+					sheetCols, err = c.GetColumns(sheetID)
+					colsPopulated = true
+					if err != nil {
+						log.Fatalf("Error: %v\n", err)
+						return nil, err //TODO: make an actual error
+					}
+
+					//perform basic validation
+					err = ValidateCellsInRow(r.Cells, sheetCols, opt)
+					if err != nil {
+						log.Fatalf("Error: %v\n", err)
+						return nil, err //TODO: make an actual error
+					}
+				}
+
+				r.Cells[j].ColumnID = sheetCols[j].ID
+			}
 		}
 
 		//adjust row options
-		switch opt {
+		switch rowOpt {
 		case ToBottom:
 			r.ToBottom = true
 		case ToTop:
@@ -240,27 +241,28 @@ func (c *Client) AddRowsToSheet(sheetID string, opt RowPostOptions, rows []Row) 
 	return body, nil
 }
 
-func (c *Client) PostObjects(path string, jsonWrapper string, data ...interface{}) (io.ReadCloser, error) {
+//Pending deletion
+// func (c *Client) PostObjects(path string, jsonWrapper string, data ...interface{}) (io.ReadCloser, error) {
 
-	//build data array
-	log.Println("post")
-	jsonData := make([]interface{}, len(data))
-	for i, d := range data {
-		b := new(bytes.Buffer)
-		err := json.NewEncoder(b).Encode(d)
-		if err != nil {
-			log.Fatalf("Failed to encode: %v\n", err)
-		}
-		jsonData[i] = b.String()
-	}
-	//apply format to wrapper
-	bodyData := new(bytes.Buffer)
-	fmt.Fprintf(bodyData, jsonWrapper, jsonData...)
+// 	//build data array
+// 	log.Println("post")
+// 	jsonData := make([]interface{}, len(data))
+// 	for i, d := range data {
+// 		b := new(bytes.Buffer)
+// 		err := json.NewEncoder(b).Encode(d)
+// 		if err != nil {
+// 			log.Fatalf("Failed to encode: %v\n", err)
+// 		}
+// 		jsonData[i] = b.String()
+// 	}
+// 	//apply format to wrapper
+// 	bodyData := new(bytes.Buffer)
+// 	fmt.Fprintf(bodyData, jsonWrapper, jsonData...)
 
-	log.Printf("Body:\n%v\n", string(bodyData.Bytes()))
+// 	log.Printf("Body:\n%v\n", string(bodyData.Bytes()))
 
-	return c.Post(path, bodyData)
-}
+// 	return c.Post(path, bodyData)
+// }
 
 func (c *Client) PostSingleObject(path string, data interface{}) (io.ReadCloser, error) {
 
@@ -279,6 +281,30 @@ func (c *Client) PostSingleObject(path string, data interface{}) (io.ReadCloser,
 func (c *Client) Post(path string, body io.Reader) (io.ReadCloser, error) {
 
 	req, err := http.NewRequest("POST", c.url+"/"+path, body)
+
+	if err != nil {
+		log.Fatalln("Failed: %v", err)
+		return nil, err
+	}
+
+	log.Printf("URL: %v\n", req.URL)
+
+	req.Header.Add("Authorization", "Bearer "+c.apiKey)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		log.Fatalln("Failed: %v", err)
+		return nil, err
+	}
+
+	//TODO: check resp.StatusCode?
+	return resp.Body, nil
+}
+
+//TODO: consolidate PUT and post
+func (c *Client) Put(path string, body io.Reader) (io.ReadCloser, error) {
+
+	req, err := http.NewRequest("PUT", c.url+"/"+path, body)
 
 	if err != nil {
 		log.Fatalln("Failed: %v", err)
---
+++ options.go
@@ -0,0 +1,45 @@
+package goSmartSheet
+
+import "errors"
+
+//RowPostOptions is used in conjuction with Adding a Row to a sheet to specific location
+type RowPostOptions int16
+
+const (
+	//ToTop will Add or move the Row to the top of the Sheet.
+	ToTop RowPostOptions = iota
+	//ToBottom will Add or move the Row to the bottom of the Sheet.
+	ToBottom
+	//Above will Add or move the Row directly above the specified sibling Row (at the same hierarchical level).
+	//Sibling Row must be populated for this option to work
+	Above
+)
+
+//PostOptions is used during a post to control the level of validation / adjustments performed by the client
+type PostOptions int16
+
+const (
+	//NormalValidation performs the default logic for that specific operation
+	NormalValidation PostOptions = 1 << iota
+	//IgnoreColumnLengthValidation will disable all column validation and assueme the calling application build the columns / rows correctly
+	IgnoreColumnLengthValidation
+	//IgnoreRightMostColumns will fix / adjust the leading columns and then ignore the rest of the columns provided
+	IgnoreRightMostColumns
+)
+
+//ValidateCellsInRow will validate that the cells match the columns within the sheet based on the specified PostOptions
+func ValidateCellsInRow(cells []Cell, sheetCols []Column, opt PostOptions) error {
+	switch opt {
+	case NormalValidation:
+		if len(sheetCols) != len(cells) {
+			return errors.New("Cells within a row  must match columns in sheet")
+		}
+	case IgnoreRightMostColumns:
+		//only validate that it does not have more columns
+		if len(sheetCols) < len(cells) {
+			return errors.New("Cells within a row cannot be greater than the columns within the sheet")
+		}
+	}
+
+	return nil
+}
