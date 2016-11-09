4ce54ecef9ac3384690320c727b6497c358af87e
“Taylor <Taylor.Barrick@concur.com>
2016-11-08 20:14:14 -0600

    Moved to Assert, removes explicit reflection

--- cell_test.go
+++ cell_test.go
@@ -1,39 +1,35 @@
 package goSmartSheet
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMarshalJSON(t *testing.T) {
+	assert := assert.New(t)
 	var c1, c2, c3 CellValue
-	c1.IntVal = 5
-	c2.StringVal = "HEY"
-	c3.FloatVal = 1.34
 	var err error
 	var b []byte
+
+	c1.IntVal = 5
 	if b, err = c1.MarshalJSON(); err != nil {
 		t.Error(err)
 	} else {
-		expected := []byte{53} // [53] = "5"
-		if !reflect.DeepEqual(b, expected) {
-			t.Errorf("Expected %v and got %v", expected, b)
-		}
+		assert.Equal(b, []byte{53}, "Should be equal to 53 or \"5\"")
 	}
+
+	c2.StringVal = "HEY"
 	if b, err = c2.MarshalJSON(); err != nil {
 		t.Error(err)
 	} else {
-		expected := []byte{34, 72, 69, 89, 34} // [34 72 69 89 34] = \"HEY\"
-		if !reflect.DeepEqual(b, expected) {
-			t.Errorf("Expected %v and got %v", expected, b)
-		}
+		assert.Equal(b, []byte{34, 72, 69, 89, 34}, "Should be equal to {34 72 69 89 34} or \"HEY\"")
 	}
+
+	c3.FloatVal = 1.34
 	if b, err = c3.MarshalJSON(); err != nil {
 		t.Error(err)
 	} else {
-		expected := []byte{49, 46, 51, 52} // [49 46 51 52] = "1.34"
-		if !reflect.DeepEqual(b, expected) {
-			t.Errorf("Expected %v and got %v", expected, b)
-		}
+		assert.Equal(b, []byte{49, 46, 51, 52}, "Should be equal to {49, 46, 51, 52} or \"1.34\"")
 	}
 }
